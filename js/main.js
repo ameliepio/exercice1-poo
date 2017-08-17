a9433720684c737ba432d34c38afcf00b089c831
Amelie <a.pioger@outlook.fr>
2017-08-17 11:58:51 +0200

    ex1 finish

--- js/main.js
+++ js/main.js
@@ -1,27 +1,66 @@
 // exercice 1
-var nico = new Person('nico','Ponthieux',1984,'francaise','cb',[]);
+var amel = new Person('amelie','Pioger',1980,'france','visa','date','year',[]);
+// var year=(amel.date-amel.age);
 
-
-function Person(nick, nom, naissance, origine, paiement){
-  this.nick = nick;
+function Person(prenom, nom, age, pays, paiement,date,){
+  this.prenom = prenom;
   this.nom = nom;
-  this.naissance = naissance;
-  this.origine = origine;
-  this.paiement = paiement;
+  this.age = age;
+  this.pays = pays;
+  this.paiement = ["visa"] ;
   this.date=2017;
+  this.year=(this.date-this.age);
+};
+alert(amel.prenom+' '+amel.nom+ ' '+amel.year+''+'ans')
+
+// alert(amel.year)
+// alert(amel.date-amel.age+" ans ")
+
+/*Comparaison*/
+var comp=new Comparaison(['FRANCE','BELGIQUE','ALLEMAGNE',],18,'MASTERCARD');
+function Comparaison(pays,year,paiement){
+this.pays=pays,
+this.year=year,
+this.paiement = paiement,
+this.compare=function(){
+
+};
+};
+
+// /*Age*/
+//
+if (comp.year<amel.year){
+  alert('acces autorise');
+}else {
+  alert ('acces refuse');
 }
 
+/*Pays*/
 
-alert(nico.nick+' '+nico.nom);
+var cpt=0
+for (let i = 0; i < comp.pays.length; i++) {
+  if (amel.pays===comp.pays[i]) {
+cpt++;
+break;}
+}
+if(cpt){
+    alert('pays autorise');
+}else {
 
+  alert('pays refuse');
+};
 
-alert(nico.date-nico.naissance+" ans ");
+/*Paiement*/
 
+var cptp=0
+for (let j = 0; j < comp.paiement.length; j++) {
+  if (amel.paiement===comp.paiement[j]) {
+cptp++;
+break;}
+}
+if(cptp){
+    alert('paiement autorise');
+}else {
 
-
-
-    // la liste tout en majuscules des pays autorisés pour notre utilisateur
-    //
-    // la liste tout en majuscules des moyens de paiment autorisés sur le site
-    //
-    // l'âge minimum pour entrer sur le site
+  alert('paiement refuse');
+};
